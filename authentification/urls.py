3dd6dd9512f4b4e0e490f896526e21dcdb5cdcc8
faly9 <randriantsoafaly8@gmail.com>
2025-05-11 09:59:31 +0000

    urls.py

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from authentification.views import connexion,register,deconnection
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('register/' , register , name='register'),
-=======
     path('' , register , name='register'),
->>>>>>> faly
     path('login/' , connexion , name = 'connexion'),
     path('logout/' , deconnection , name='deconnexion')
 ]
